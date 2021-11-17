52d91abf3202b17b85a73ac33a4c9efab286e9c7
Mehdi BOUDOUR <mehdiboudour.contact@gmail.com>
2021-11-17 22:34:40 +0100

    Consuming a RESTful Web Service

--- src/main/java/springboot/hello/hellospringboot/Application.java
+++ src/main/java/springboot/hello/hellospringboot/Application.java
@@ -1,27 +1,36 @@
 package springboot.hello.hellospringboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
         return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for(String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random",
+                    Quote.class);
+            log.info(quote.toString());
         };
     }
 }
